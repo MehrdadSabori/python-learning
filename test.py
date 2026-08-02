20a65072e4dfae82c0d166d9520661ab89046e21
MehrdadSabori <mehrdad.sabori76@gmail.com>
2026-08-02 18:23:21 +0330

    if, elif, else

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
-userName = input("enter your name: ")
-userAge = int(input("enter your age: "))
-yourBirthday = 2026 - userAge
-print(userName, userAge, yourBirthday)
+# userName = input("enter your name: ")
+# userAge = int(input("enter your age: "))
+# yourBirthday = 2026 - userAge
+# print(userName, userAge, yourBirthday)
+# --------------------
+yourAge = int(input('enter your age: '))
+if yourAge <= 12:
+    print('child')
+elif yourAge <= 17:
+    print('teenager')
+else:
+    print('adult')
